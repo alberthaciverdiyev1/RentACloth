6f568f5a1726e027acc172b5982d0d46e8794683
alberthaciverdiyev1 <alberthaciverdiyev55@gmail.com>
2025-01-08 14:59:48 +0400

    Moved ViewController to Web\ViewController and add login method for authentication view

--- app/Http/Controllers/Web/ViewController.php
+++ app/Http/Controllers/Web/ViewController.php
@@ -15,7 +15,9 @@ class ViewController extends Controller
     }
     public function aboutUs(){
         $about = AboutUs::first();
-
         return view('about/index', compact('about'));
+    }
+    public function login(){
+        return view('auth/login');
     }
 }
